d8d59af796c40847f491c6bd34979e7784aa36c3
devdoubled <duylvd2002@gmail.com>
2024-09-14 13:12:53 +0700

    14/09/24-DuyLVD Config tailwindCSS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,10 @@ function Home() {
   return (
     <div className={cx("home-wrapper")}>
       <div className={cx("home-container")}>
-        Hôm nay tôi buồn
+        <h1 className={cx("text-4xl font-bold text-blue-500")}>
+          Hello, React with Tailwind CSS and Custom SCSS!
+        </h1>
+        <p className={cx("mb-3 font-normal text-gray-700")}>This is styled with custom SCSS classes!</p>
       </div>
     </div>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Add paths to all of your template files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
