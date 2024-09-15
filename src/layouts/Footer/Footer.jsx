f73fecafae20963fcdf285590aff555250cbc8fa
devdoubled <duylvd2002@gmail.com>
2024-09-16 00:18:35 +0700

    15/09/24-DuyLVD Testing new update CICD

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div>
+      <footer className="text-center p-4" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
+        <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+          Copyright   2022 - VSAT Learning Center
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
