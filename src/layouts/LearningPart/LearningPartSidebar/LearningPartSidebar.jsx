1899234ff71877a9f3ebcbcb10c85064264fa2cf
devdoubled <duylvd2002@gmail.com>
2024-09-28 10:41:37 +0700

    28/09/24-DuyLVD Create UI LearningPart and LearingPartDetail

---
+++ src/layouts/LearningPart/LearningPartSidebar/LearningPartSidebar.jsx
@@ -0,0 +1,45 @@
+import classNames from "classnames/bind";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import LMImg from "../../../assets/images/content/lm-01.png";
+import PartUnitItem from "../../../components/LearningPart/LearningPartSidebar/PartUnitItem";
+import styles from "./LearningPartSidebar.module.scss";
+const cx = classNames.bind(styles);
+
+function LearningPartSidebar({ learningContent }) {
+  const content =
+    learningContent === "sat-reading-and-writing"
+      ? "SAT Reading and Writing"
+      : "SAT Math";
+  return (
+    <div className={cx("learning-part-sidebar-container")}>
+      <Link to="/learning/part" className={cx("learning-part-introduction", "introduction-active")}>
+        <img
+          src={LMImg}
+          alt="introduction-img"
+          className={cx("introduction-img")}
+        />
+        <div className={cx("introduction-infor")}>
+          <div className={cx("infor-title")}>Digital {content} </div>
+          <div className={cx("infor-details")}>
+            <span className={cx("infor-units")}>11 UNITS</span>
+            {" - "}
+            <span className={cx("infor-skills")}>41 SKILLS</span>
+          </div>
+        </div>
+      </Link>
+      <div className={cx("learning-part-unit-list")}>
+        <PartUnitItem
+          item="About the digital SAT"
+          learningContent={learningContent}
+        />
+      </div>
+    </div>
+  );
+}
+
+LearningPartSidebar.propTypes = {
+  learningContent: PropTypes.string.isRequired,
+};
+
+export default LearningPartSidebar;
--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,6 +1,9 @@
 import About from '../pages/About';
 import Contact from '../pages/Contact/Contact';
 import Home from '../pages/Home';
+import Learning from '../pages/Learning';
+import LearningPart from '../pages/LearningPart';
+import LearningPartDetail from '../pages/LearningPartDetail/LearningPartDetail';
 import News from '../pages/News/News';
 import NotFound from '../pages/NotFound';
 import Profile from '../pages/Profile';
@@ -11,6 +14,10 @@ const routesConfig = [
   { path: "/contact", component: Contact },
   { path: "/news", component: News },
   { path: '/profile', component: Profile, protected: true },
+  { path: '/learning', component: Learning, protected: true },
+  { path: '/learning/:slug', component: LearningPart, protected: true },
+  { path: '/learning/:slug/:idSection', component: LearningPart, protected: true },
+  { path: '/learning/:slug/:idSection/:id', component: LearningPartDetail, protected: true },
   { path: "*", component: NotFound },
 ];
 
