a018f2d4e0c237ef35fb08a2e6b2c02e253e75c2
khanhncd2008 <132860339+khanhncd2008@users.noreply.github.com>
2024-09-29 21:29:43 +0700

    29/09/24-KhanhNCD Create UI About Us

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,16 @@
-import classNames from "classnames/bind"
-import Header from "../../layouts/Header/Header"
-import styles from "./Contact.module.scss"
+import classNames from "classnames/bind";
+import Header from "../../layouts/Header/Header";
+import Footer from "../../layouts/Footer/Footer";
+import styles from "./Contact.module.scss";
 const cx = classNames.bind(styles)
 function Contact() {
   return (
     <div className={cx("contact-wrapper")}>
       <Header />
+      <div className={cx("contact-container")}>
+
+      </div>
+      <Footer />
     </div>
   )
 }
