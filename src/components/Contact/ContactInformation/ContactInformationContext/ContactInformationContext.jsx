2962a771f81acdbcb9f90a7e977e006d613dca80
khanhncd2008 <132860339+khanhncd2008@users.noreply.github.com>
2024-10-03 07:12:39 +0700

    03/10/24-KhanhNCD Update UI Contact Us

--- src/components/Contact/ContactInformation/ContactInformationContext/ContactInformationContext.jsx
+++ src/components/Contact/ContactInformation/ContactInformationContext/ContactInformationContext.jsx
@@ -2,15 +2,15 @@ import classNames from "classnames/bind";
 import styles from "./ContactInformationContext.module.scss"
 const cx = classNames.bind(styles)
 
-const ContactInfo = () => {
+const ContactInformationContext = () => {
     return (
-      <div className="contact-context-container">
-        <div className="contact-context-title">Get In Touch With Us</div>
-            <div className={cx("contact-context")}>
-                We’re here to help! Whether you have questions about our SAT preparation platform, need support, or would like to learn more about how VSAT can help you achieve your academic goals, don’t hesitate to reach out.
-            </div>
+      <div className={cx("contact-context-wrapper")}>
+        <div className={cx("contact-context-title")}>Get In Touch With Us</div>
+        <div className={cx("contact-context-description")}>
+            We’re here to help! Whether you have questions about our SAT preparation platform, need support, or would like to learn more about how VSAT can help you achieve your academic goals, don’t hesitate to reach out.
+        </div>
       </div>
     );
   };
   
-  export default ContactInfo;
+  export default ContactInformationContext;
