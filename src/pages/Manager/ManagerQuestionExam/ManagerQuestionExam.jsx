24c792837dacecbb5ed85090208e593bbb8100c9
khanhncd2008 <132860339+khanhncd2008@users.noreply.github.com>
2024-10-12 08:06:09 +0700

    Fix Layout Manager

--- src/pages/Manager/ManagerQuestionExam/ManagerQuestionExam.jsx
+++ src/pages/Manager/ManagerQuestionExam/ManagerQuestionExam.jsx
@@ -1,5 +1,5 @@
 import classNames from "classnames/bind";
-import PageLayout from "~/layouts/manager/PageLayout";
+import PageLayout from "~/layouts/Manager/PageLayout";
 import styles from "./ManagerQuestionExam.module.scss";
 const cx = classNames.bind(styles);
 function QuestionExam() {
