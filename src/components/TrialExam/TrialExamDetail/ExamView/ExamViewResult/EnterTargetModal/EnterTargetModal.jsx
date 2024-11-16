789510b57d93eb189dfe103cf348a4531748a369
devdoubled <duylvd2002@gmail.com>
2024-11-16 20:39:15 +0700

    16/11/24-DuyLVD Create Learning Target

--- src/components/TrialExam/TrialExamDetail/ExamView/ExamViewResult/EnterTargetModal/EnterTargetModal.jsx
+++ src/components/TrialExam/TrialExamDetail/ExamView/ExamViewResult/EnterTargetModal/EnterTargetModal.jsx
@@ -1,7 +1,7 @@
 import classNames from "classnames/bind";
 import styles from "./EnterTargetModal.module.scss";
 const cx = classNames.bind(styles);
-function EnterTargetModal({ setShowEnterTarget }) {
+function EnterTargetModal() {
   return (
     <div className={cx("enter-target-popup-wrapper")}>
       <div className={cx("enter-target-popup-container")}>
