138cc96109b9b021d0454942ded8e2f6bb2ff597
devdoubled <duylvd2002@gmail.com>
2024-11-16 20:38:37 +0700

    16/11/24-DuyLVD Create Learning Target

--- src/pages/TrialExam/TrialExam.jsx
+++ src/pages/TrialExam/TrialExam.jsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@mui/material";
 import classNames from "classnames/bind";
 import { useCallback, useEffect, useState } from "react";
 import TrialExamItem from "~/components/TrialExam/TrialExamItem";
@@ -7,16 +8,17 @@ import styles from "./TrialExam.module.scss";
 const cx = classNames.bind(styles);
 function TrialExam() {
   const [examList, setExamList] = useState([]);
+  const [isWaiting, setIsWaiting] = useState(false)
   const fetchExamList = useCallback(async () => {
     try {
       const examName = "Trial Exam"
-      // setIsWaiting(true);
+      setIsWaiting(true);
       const response = await apiClient.get(`/exams/getExamByExamType/${examName}`);
       setExamList(response.data.data);
     } catch (error) {
       console.error("Failed to fetch exam structure list:", error);
     } finally {
-      // setIsWaiting(false);
+      setIsWaiting(false);
     }
   }, []);
 
@@ -27,9 +29,23 @@ function TrialExam() {
     <div className={cx("trial-exam-wrapper")}>
       <HeaderAuthen />
       <div className={cx("trial-exam-container")}>
-        {examList?.map((exam, index) => (
-          <TrialExamItem key={exam.id} index={index + 1} exam={exam} />
-        ))}
+        {isWaiting ? (
+          <>
+            {[...Array(3)].map((_, i) => (
+              <Skeleton
+                key={i}
+                animation="wave"
+                variant="rectangular"
+                width="100%"
+                height={317}
+              />
+            ))}
+          </>
+        ) : (
+          examList?.map((exam, index) => (
+            <TrialExamItem key={exam.id} index={index + 1} exam={exam} />
+          ))
+        )}
       </div>
     </div>
   );
