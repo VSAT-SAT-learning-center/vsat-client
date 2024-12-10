cc0cf202321bbb002e1a7409fd6402caf5b79f27
devdoubled <duylvd2002@gmail.com>
2024-12-10 10:52:49 +0700

    10/12/24-DuyLVD Fix bug

--- src/data/About/informationOurInstructor.js
+++ src/data/About/informationOurInstructor.js
@@ -1,29 +1,31 @@
-import KhanhImage from "~/assets/images/content/Khanh.jpg";
-import MDuyImage from "~/assets/images/content/MDuy.jpg";
+import CCongImage from "~/assets/images/content/congcao.png";
+import DevDDImage from "~/assets/images/content/devdd.png";
+import KhanhImage from "~/assets/images/content/Khanh.png";
+import MDuyImage from "~/assets/images/content/MDuy.png";
 
 export const informationOurInstructor = [
-    {
-      id: 1,
-      image: MDuyImage,
-      name: "Duy Le",
-      job: "UI/UX Designer",
-    },
-    {
-        id: 2,
-        image: KhanhImage,
-        name: "Khanh Nguyen",
-        job: "UI/UX Designer",
-    },
-    {
-        id: 3,
-        image: MDuyImage,
-        name: "Duy Nguyen",
-        job: "UI/UX Designer",
-    },
-    {
-        id: 4,
-        image: MDuyImage,
-        name: "Cong Cao",
-        job: "UI/UX Designer",
-    }
+  {
+    id: 1,
+    image: DevDDImage,
+    name: "Duy Le",
+    job: "Leader",
+  },
+  {
+    id: 2,
+    image: KhanhImage,
+    name: "Khanh Nguyen",
+    job: "Member",
+  },
+  {
+    id: 3,
+    image: MDuyImage,
+    name: "Duy Nguyen",
+    job: "Member",
+  },
+  {
+    id: 4,
+    image: CCongImage,
+    name: "Cong Cao",
+    job: "Member",
+  },
 ];
